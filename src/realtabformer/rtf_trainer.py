7f9d7837edbbcfa38092f35ea616a4252ee2d53b
Vukasin Toroman <vukasin@toroman.name>
2024-11-21 23:20:39 +0100

    fix args name mismatch (#91)
    * fix args name mismatch
    
    * add devcontainer config
    
    * update transformers dependency
    
    * remove devcontainer config

--- src/realtabformer/rtf_trainer.py
+++ src/realtabformer/rtf_trainer.py
@@ -81,17 +81,17 @@ class ResumableTrainer(Trainer):
         callbacks.append(SaveEpochEndCallback(save_epochs=save_epochs))
 
         super().__init__(
-            model,
-            args,
-            data_collator,
-            train_dataset,
-            eval_dataset,
-            tokenizer,
-            model_init,
-            compute_metrics,
-            callbacks,
-            optimizers,
-            preprocess_logits_for_metrics,
+            model=model,
+            args=args,
+            data_collator=data_collator,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            processing_class=tokenizer,
+            model_init=model_init,
+            compute_metrics=compute_metrics,
+            callbacks=callbacks,
+            optimizers=optimizers,
+            preprocess_logits_for_metrics=preprocess_logits_for_metrics,
         )
         self.target_epochs = target_epochs
 
